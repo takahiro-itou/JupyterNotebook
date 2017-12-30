d5aa09832fa8cbac0685f2d1d165379f60fc9ea2
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2017-12-30 11:43:44 +0900

    Python のインポートのテスト
    (1) 関数内でインポートを行った場合、キャッシュされるか

---
+++ Python3/ToyCode/ImportTest/test1.py
@@ -0,0 +1,9 @@
+
+counter = 0
+
+def  print_msg():
+    global  counter
+    counter += 1
+    print('This is Test1.', counter)
+# End Def (print_msg)
+
---
+++ Python3/ToyCode/ImportTest/test2.py
@@ -0,0 +1,9 @@
+
+counter = 0
+
+def  print_msg():
+    global  counter
+    counter += 100
+    print('This is Test2.', counter)
+# End Def (print_msg)
+
